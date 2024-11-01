125ec54342234c25e5f58075ffb458c89e6f558f
Sander Holm <80288213+holmsander@users.noreply.github.com>
2024-11-01 15:14:33 +0100

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -12,7 +12,7 @@ const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQL_ROOT_PASSWORD,
-  database: process.env.MYSQLDATABASE,
+  database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQLPORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
